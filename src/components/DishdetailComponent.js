d0ed478683631c44c2230ceb5737a047afb4a925
sikruger <Simon.Krueger@t-systems.com>
2020-11-30 11:33:23 +0100

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -13,7 +13,7 @@ import {
 } from "reactstrap";
 import {Link} from "react-router-dom";
 import {Control, Errors, LocalForm} from "react-redux-form";
-
+import { Loading } from './LoadingComponent';
 
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -155,7 +155,25 @@ const minLength = (len) => (val) => val && (val.length >= len);
 
 
     const DishDetail = (props) => {
-        if (props.dish != null) {
+        if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null) {
             return (
                 <div className="container">
                     <div className="row">
