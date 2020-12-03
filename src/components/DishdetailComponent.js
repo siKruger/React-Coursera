9f6b36e06a82f230c510710d1b3abbbd88ae6ada
sikruger <Simon.Krueger@t-systems.com>
2020-12-03 14:09:44 +0100

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -14,7 +14,7 @@ import {
 import {Link} from "react-router-dom";
 import {Control, Errors, LocalForm} from "react-redux-form";
 import { Loading } from './LoadingComponent';
-
+import { baseUrl } from '../shared/baseUrl';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -115,7 +115,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
     function RenderDish({dish}) {
         return (
             <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>
